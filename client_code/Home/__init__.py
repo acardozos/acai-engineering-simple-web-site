97bc627efc5cb7a81363ddcf3be65ad279003301
ajcs.developer@gmail.com <ajcs.developer@gmail.com>
2023-12-13 16:34:12 +0000

    Created Home; Edited Main

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,11 +1,9 @@
-from ._anvil_designer import MainTemplate
+from ._anvil_designer import HomeTemplate
 from anvil import *
-from ..Home import Home
 
-class Main(MainTemplate):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home(), full_width_row=True)
 
     # Any code you write here will run before the form opens.
