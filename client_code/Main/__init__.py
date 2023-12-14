d7de4f954e190b68caf56e142a04f44dc710a85e
ajcs.developer@gmail.com <ajcs.developer@gmail.com>
2023-12-14 23:14:25 +0000

    Edited Home, Main, assets/css/style.css, landpage.html and 1 more; Deleted theme.css and standard-page.html

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -6,6 +6,5 @@ class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home(), full_width_row=True)
 
-    # Any code you write here will run before the form opens.
+    self.content_panel.add_component(Home(), full_width_row=True)
