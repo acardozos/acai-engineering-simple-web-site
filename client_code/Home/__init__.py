7016dbf91c6a06d39f68c1a53d77391de1849718
ajcs.developer@gmail.com <ajcs.developer@gmail.com>
2023-12-15 04:08:41 +0000

    Edited settings, landpage.html and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -7,3 +7,7 @@ class Home(HomeTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def my_method(self, name):
+    alert(f"Hello, {name}")
+    return 42
